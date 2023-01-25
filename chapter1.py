ae50caffe4ba2ac6725ccdd5ede5a98fde044a03
Rawan Khalid <81408575+Rawan-Kh@users.noreply.github.com>
2023-01-25 18:40:25 +0200

    Update chapter1.py

--- chapter1.py
+++ chapter1.py
@@ -16,3 +16,15 @@ volunteer["hits"] = volunteer["hits"].astype("int")
 
 # Look at the dtypes of the dataset
 print(volunteer.dtypes)
+
+---------------------
+# Create a data with all columns except category_desc
+X = volunteer.drop("category_desc", axis=1)
+
+# Create a category_desc labels dataset
+y = volunteer[["category_desc"]]
+# Use stratified sampling to split up the dataset according to the y dataset
+X_train, X_test, y_train, y_test = train_test_split(X,y, stratify=y ,random_state=42)
+
+# Print the category_desc counts from y_train
+print(y_train['category_desc'].value_counts())
